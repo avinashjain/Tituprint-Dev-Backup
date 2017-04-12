23e1b897435b626c6942e4488d94fbcf3846ba66
Avinash Kothari <javinash33@gmail.com>
2017-04-12 15:47:43 +0530

    updated

--- JS/tituprint.1.0.0.js
+++ JS/tituprint.1.0.0.js
@@ -1,4 +1,7 @@
+/**
+ * Created by Admin on 12/04/2017.
+ */
 $(function(){
    var _header_menu = $('header .container').find('.navbar-collapse').html();
-   alert(_header_menu);
+   console.log(_header_menu);
 });
