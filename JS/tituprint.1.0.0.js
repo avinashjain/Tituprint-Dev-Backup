8b99e9bff2e7b92f63496461f5d41801d464ab83
Avinash Kothari <javinash33@gmail.com>
2017-04-12 16:07:12 +0530

    Updated 1.1

--- JS/tituprint.1.0.0.js
+++ JS/tituprint.1.0.0.js
@@ -5,15 +5,16 @@ $(function(){
    var _header_menu = $('header .container').find('.navbar-collapse').html();
    console.log(_header_menu);
     $('header .container').find('.navbar-collapse').remove();
-    $(_header_menu).css('position','absolute');
-    $(_header_menu).css('bottom','0px');
-    $(_header_menu).css('z-index','1');
-    $(_header_menu).css('margin','0 auto');
-    $(_header_menu).css('left','0');
-    $(_header_menu).css('right','0');
-    $(_header_menu).css('overflow','hidden!important');
-    $(_header_menu).css('width','96%');
-    $(_header_menu).css('text-align','center');
-    
-    $('#top-banner').append(_header_menu);
+    var _ctrl_header_menu = $(_header_menu);
+    _ctrl_header_menu.css('position','absolute');
+    _ctrl_header_menu.css('bottom','0px');
+    _ctrl_header_menu.css('z-index','1');
+    _ctrl_header_menu.css('margin','0 auto');
+    _ctrl_header_menu.css('left','0');
+    _ctrl_header_menu.css('right','0');
+    _ctrl_header_menu.css('overflow','hidden!important');
+    _ctrl_header_menu.css('width','96%');
+    _ctrl_header_menu.css('text-align','center');
+
+    $('#top-banner').append(_ctrl_header_menu);
 });
