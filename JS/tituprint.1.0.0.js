02c605a471c968e8642f8173aeb2ae415bfd57c3
Avinash Kothari <javinash33@gmail.com>
2017-04-12 18:01:04 +0530

    Updated 1.19

--- JS/tituprint.1.0.0.js
+++ JS/tituprint.1.0.0.js
@@ -2,24 +2,23 @@
  * Created by Admin on 12/04/2017.
  */
 $(function(){
-   var _header_menu = $('header .container').find('.navbar-collapse').html();
-   console.log(_header_menu);
-   _header_menu = '<div class="navbar-collapse" id="div_header_menus">'+_header_menu+'</div>';
-    $('header .container').find('.navbar-collapse').remove();
-    var _ctrl_header_menu = $(_header_menu);
-    $('#top-banner').append(_ctrl_header_menu);
-
-    var pathname = window.location.pathname;
-    console.log('pathname: '+pathname);
-
     $('head').append('<link rel="stylesheet" href="https://cdn.rawgit.com/avinashjain/Tituprint-Dev-Backup/0e8dce23bcf88c9abda042bc76c530da15451961/CSS/1.0-footer.css" type="text/css" />');
+    var pathname = window.location.pathname;
     if(pathname == '/' || pathname.indexOf('index.html') != -1){
+        var _header_menu = $('header .container').find('.navbar-collapse').html();
+        _header_menu = '<div class="navbar-collapse" id="div_header_menus">'+_header_menu+'</div>';
+
+        $('header .container').find('.navbar-collapse').remove();
+
+        var _ctrl_header_menu = $(_header_menu);
+        $('#top-banner').append(_ctrl_header_menu);
+
         $('head').append('<link rel="stylesheet" href="https://cdn.rawgit.com/avinashjain/Tituprint-Dev-Backup/ed453968f3f70bb50d51b536de77cb54a46b29b5/CSS/1.0-custom.css" type="text/css" />');
     }
     setTimeout(function(){
         $('body').show();
     },1000);
 
-    var url = window.location.href;
-    console.log('url: '+url);
+    // var url = window.location.href;
+    // console.log('url: '+url);
 });
