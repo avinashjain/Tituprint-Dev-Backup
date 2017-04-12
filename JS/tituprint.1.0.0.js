2c7f686a3e84e25f4969b1a778d01702be104b2d
Avinash Kothari <javinash33@gmail.com>
2017-04-12 16:22:49 +0530

    Updated 1.3

--- JS/tituprint.1.0.0.js
+++ JS/tituprint.1.0.0.js
@@ -4,10 +4,11 @@
 $(function(){
    var _header_menu = $('header .container').find('.navbar-collapse').html();
    console.log(_header_menu);
+   _header_menu = '<div class="navbar-collapse">'+_header_menu+'</div>';
     $('header .container').find('.navbar-collapse').remove();
     var _ctrl_header_menu = $(_header_menu);
     _ctrl_header_menu.css('position','absolute');
-    _ctrl_header_menu.css('bottom','0px');
+    _ctrl_header_menu.css('bottom','10px');
     _ctrl_header_menu.css('z-index','1');
     _ctrl_header_menu.css('margin','0 auto');
     _ctrl_header_menu.css('left','0');
